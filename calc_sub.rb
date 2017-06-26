f1919a1b5b1e54206354bcbcf061cdc311c7f6bd
Ty-Zeta <tyofzeta@gmail.com>
2017-06-26 11:29:24 -0400

    updated subtraction with gits.chomp

--- calc_sub.rb
+++ calc_sub.rb
@@ -1,4 +1,6 @@
-def sub(a, b)
-  return a - b
-end
-p sub(10, 4)
+p "Enter first number to subtract"
+n1 = gets.chomp.to_i
+p "Enter second number to subtract"
+n2 = gets.chomp.to_i
+n3 = n1 - n2
+p "The answer to #{n1} - #{n2} = #{n3}"
